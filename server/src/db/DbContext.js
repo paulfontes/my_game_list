e94e7d7b09346194a1621c33b74dc441ffc76f4f
Paul Fontes <pgfon22@gmail.com>
2025-10-02 10:00:20 -0600

    dbcontext reviews

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,12 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
 import { SavedGameSchema } from '../models/SavedGame.js';
+import { ReviewSchema } from '../models/Review.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   SavedGames = mongoose.model('SavedGame', SavedGameSchema);
+  Reviews = mongoose.model('Review', ReviewSchema)
 }
 
 export const dbContext = new DbContext()
