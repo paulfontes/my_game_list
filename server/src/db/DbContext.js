3468b8f69e5caba670fc6dbe8923f824a6d13dce
Paul Fontes <pgfon22@gmail.com>
2025-10-02 09:51:38 -0600

    dbcontext savedgames

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
+import { SavedGameSchema } from '../models/SavedGame.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  SavedGames = mongoose.model('SavedGame', SavedGameSchema);
 }
 
 export const dbContext = new DbContext()
