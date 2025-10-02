b8f6fc33a8920015488b553a080e1b81f01e4c53
Paul Fontes <pgfon22@gmail.com>
2025-10-02 09:40:44 -0600

    Account model

--- server/src/models/Account.js
+++ server/src/models/Account.js
@@ -6,8 +6,12 @@ export const AccountSchema = new Schema(
     subs: [{ type: String }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
     // NOTE If you wish to add additional properties do so here
+    bio: { type: String, minLength: 1, maxLength: 500 },
+    coverImg: { type: String, minLength: 1, maxLength: 500 },
+    favGameName: { type: String, minLength: 1, maxLength: 300 },
+    favGameImg: { type: String, minLength: 1, maxLength: 500 },
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
