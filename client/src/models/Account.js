d917e991992be31d5997266eac81c2cd24cb5448
Paul Fontes <pgfon22@gmail.com>
2025-10-06 16:32:43 -0600

    form stuff for account

--- client/src/models/Account.js
+++ client/src/models/Account.js
@@ -1,18 +1,13 @@
 export class Account {
-  /**
-   * @typedef AccountData
-   * @property {string} id
-   * @property {string} email
-   * @property {string} name
-   * @property {string} picture
-   * 
-   * @param {AccountData} data
-   */
+
   constructor(data) {
     this.id = data.id
     this.email = data.email
     this.name = data.name
     this.picture = data.picture
-    // TODO add additional properties if needed
+    this.bannerImg = data.bannerImg
+    this.favGameImg = data.favGameImg
+    this.favGameName = data.favGameName
+    this.bio = data.bio
   }
 }
--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -1,26 +1,96 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { AppState } from '../AppState.js';
 
 const account = computed(() => AppState.account)
 
+
+const formData = ref({
+
+  name: account.value.name,
+  picture: account.value.picture,
+  bio: account.value.bio,
+  bannerImg: account.value.bannerImg
+
+})
+
+
+
+
+
+
+
+
 </script>
 
 <template>
-  <div class="about text-center">
-    <div v-if="account">
-      <h1>Welcome {{ account.name }}</h1>
-      <img class="rounded" :src="account.picture" alt="" />
-      <p>{{ account.email }}</p>
-    </div>
-    <div v-else>
-      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
-    </div>
-  </div>
+  <main class="container-fluid back-img">
+    <section class="row justify-content-center pt-5">
+      <div class="col-4 text-white">
+        <h1>{{ account.name }}'s Account</h1>
+      </div>
+      <div class="col-4">
+        <img :src="account.picture" class="profile-pic" alt="">
+      </div>
+      <div class="col-8 mt-4 text-white">
+        <form action="">
+          <span class="d-flex gap-5">
+            <div class="w-50">
+              <label for="username">Username:</label>
+              <input type="text" class="form-control">
+            </div>
+            <div class="w-50">
+              <label for=""><span class="mdi mdi-arrow-up-bold-outline"></span> Profile Picture:</label>
+              <input type="text" class="form-control">
+            </div>
+          </span>
+          <div class="mt-4">
+            <label for="">Profile Bioligio:</label>
+            <textarea name="" id="" class="form-control bio-box"></textarea>
+          </div>
+          <div class="mt-4">
+            <label for="">Profile Banner:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="text-end">
+            <button @submit.prevent="" class="btn btn-success mt-2">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <section class="row">
+        <div class="col text-center text-white">
+          <h5><span class="mdi mdi-arrow-down-bold-outline"></span> Banner Preview <span
+              class="mdi mdi-arrow-down-bold-outline"></span></h5>
+          <img class="banner-img my-4"
+            src="https://www.altavia.hu/wp-content/uploads/2020/11/Hero-Banner-Placeholder-Light-1024x480-1.png" alt="">
+        </div>
+      </section>
+    </section>
+  </main>
 </template>
 
 <style scoped lang="scss">
-img {
-  max-width: 100px;
+.profile-pic {
+  border-radius: 50%;
+}
+
+.back-img {
+  min-height: 500px;
+  background-image: linear-gradient(to bottom, rgba(2, 2, 2, 0.3)55%, rgb(0, 0, 0)), url(../assets/img/green-background.png);
+  background-position: top;
+  background-size: cover;
+}
+
+.bio-box {
+  height: 200px;
+}
+
+.banner-img {
+  border-radius: 20px;
+  height: 200px;
+  width: 67%;
+  object-fit: cover;
 }
 </style>
